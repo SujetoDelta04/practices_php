feadef555d0a588e9c15f14fb86a02eccee0abfe
Alfredo Melo <alfremelo9@gmail.com>
2024-02-27 15:50:59 +0100

    Se creo una clase para manejar la tabla de productos, de forma que ahora en el index se pueden apreciar los productos existentes

--- problema_1/functions/productos.php
+++ problema_1/functions/productos.php
@@ -14,6 +14,17 @@ class productos
 
         return $this->pdo;
     }
+    public function view_all_products()
+    {
+        $pdo=$this->conection();
+        $sql_all_products="SELECT productos.id, productos.nombre, productos.valor_unitario, productos.cantidad, categorias.nombre FROM productos JOIN categorias ON productos.categorias_id=categorias.id";
+
+        $view=$pdo->prepare($sql_all_products);
+        $view->execute();
+        $view_all=$view->fetchAll(PDO::FETCH_ASSOC);
+
+        return $view_all;
+    }
 }
 
 ?>
--- problema_1/views/index.php
+++ problema_1/views/index.php
@@ -1,87 +1,113 @@
+<?php require_once "../functions/productos.php"; ?>
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Starter</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Starter</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="../vendor/almasaeed2010/adminlte/plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="../vendor/almasaeed2010/adminlte/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+    <div class="wrapper">
 
-<?php include("includes/navbar.php") ?>
+        <?php include("includes/navbar.php") ?>
 
-<?php include("includes/aside.php") ?>
+        <?php include("includes/aside.php") ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Listado de Productos disponibles</h1>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <div class="content">
+                <div class="container-fluid">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <td>#</td>
+                                <td>Nombre del producto</td>
+                                <td>Valor</td>
+                                <td>Cantidad</td>
+                                <td>Categoria</td>
+                                <td>Opciones</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                            $products=new productos();
+                            $products_all=$products->view_all_products();
+
+                            foreach ($products_all as $fila) {
+                             ?>
+                           <tr>
+                                <td><?php echo $fila["id"] ?></td>
+                                <td><?php echo $fila["nombre"] ?></td>
+                                <td><?php echo $fila["valor_unitario"] ?></td>
+                                <td><?php echo $fila["cantidad"] ?></td>
+                                <td><?php echo $fila["nombre"] ?></td>
+                                <td></td>
+                           </tr>
+                           <?php } ?>
+                        </tbody>
+                    </table>
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+            <div class="p-3">
+                <h5>Title</h5>
+                <p>Sidebar content</p>
+            </div>
+        </aside>
+        <!-- /.control-sidebar -->
+
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="float-right d-none d-sm-inline">
+                Anything you want
+            </div>
+            <!-- Default to the left -->
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        </footer>
     </div>
-    <!-- /.content-header -->
+    <!-- ./wrapper -->
 
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-       
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    <!-- REQUIRED SCRIPTS -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="../vendor/almasaeed2010/adminlte/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../vendor/almasaeed2010/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js"></script>
+    <!-- jQuery -->
+    <script src="../vendor/almasaeed2010/adminlte/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../vendor/almasaeed2010/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
